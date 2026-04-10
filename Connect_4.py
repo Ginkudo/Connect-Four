7716f5ba8ca7637a34f02c9d9d5835600913833c
Jimmy Phan <phanjimmy02@gmail.com>
2026-04-10 13:48:35 -0400

    Player 2 selection and drop pieces functions

--- Connect_4.py
+++ Connect_4.py
@@ -1,8 +1,24 @@
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 def create_board():
     board = np.zeros((6,7))
     return board
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+def print_board(board):
+    print(np.flip(board, 0))
 board = create_board()
 game_over = False
 turn = 0
@@ -10,7 +26,17 @@ turn = 0
 while not game_over:
     # Ask for Player 1 Input
     if turn == 0:
-        selection = int(input("Player 1 Make your Selection (0-6):"))
-
-        print(selection)
-        print(type(selection))
+        col = int(input("Player 1 Make your Selection (0-6):"))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+    else:
+        # Ask for Player 2 Input
+        col = int(input("Player 2 Make your Selection (0-6):"))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+    print_board(board)
+    
+    turn += 1
+    turn = turn % 2 
